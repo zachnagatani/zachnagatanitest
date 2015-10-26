307d157a235023f26bdcae7e17355a683b83994e
znagatani <znagatani@gmail.com>
2015-10-25 19:12:19 -0700

    check if form works

--- contact.php
+++ contact.php
@@ -68,6 +68,68 @@
 	          </div><!-- col -->
 	        </div><!-- row -->
 
+		<?php 
+
+			// Check for header injections
+			function has_header_injection($str) {
+				return preg_match( "/[\r\n]/", $str );
+			}
+
+			if (isset ($_POST['contact-submit'])) {
+
+				$name = trim($_POST['name']);
+				$email = trim($_POST['email']);
+				$timeline = trim($_POST['timeline']);
+				$budget = trim($_POST['budget']);
+				$company = trim($_POST['company']);
+				$website = trim($_POST['website']);
+				$msg = trim($_POST['message']);
+
+				// Check to see if $name or $email have header injections
+				if (has_header_injection($name) || has_header_injection($email) || has_header_injection($timeline) || has_header_injection($budget) || has_header_injection($company) || has_header_injection($website)) {
+					die(); // If true, kill the script
+				}
+
+				if ( !$name || !$email || !$timeline || !$budget || !$message ) {
+
+					echo '<h4 class="raleway">Please fill all the required fields</h4><a href="contact.php" class="btn btn-block btn-lg btn-cta">Go back and try again!</a>';
+					exit;
+				}
+
+				// Add the recipient email to a variable
+				$to = "zachnagatani@gmail.com"
+
+				// Create a subject
+				$subject = "$name sent you a project inquiry";
+
+				// Construct the message
+				$message = "Name: $name\r\n";
+				$message .= "Email: $email\r\n";
+				$message .= "Timeline: $timeline\r\n";
+				$message .= "Budget: $company\r\n";
+				$message .= "Company: $company\r\n";
+				$message .= "Website: $website\r\n";
+				$message .= "Message: \r\n$msg";
+
+				$message = wordwrap($message, 72);
+
+				// Set the mail headers into a variable
+				$headers = "MIME-Version: 1.0\r\n";
+				$headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+				$headers .= "From: $name <$email>\r\n";
+				$headers .= "X-Priority: 1\r\n";
+				$headers .= "X-MSMail-Priority: High\r\n\r\n";
+
+				// Send the email!
+				mail($to, $subject, $message, $headers);
+
+		?>
+
+			<!-- Show success message after email has sent -->
+			<h4 class="raleway">Thanks for contacting me!</br>I'll get back to you shortly</h4>
+
+		<?php } else { ?>
+
 	   <form class="clearfix" role="form" method="post" action="" id="contact-form">
 			<div class="row contact-row-top">
 				<div class="col-md-3 contact-col">
@@ -102,6 +164,8 @@
 				</div><!-- col -->
 			</div><!-- row -->
 		</form>
+
+		<?php } ?>
 		</div><!-- container -->
 	</section><!-- contact -->
 

39484d6f027ea98af03b1228ece4d7cec7ec75dc
znagatani <znagatani@gmail.com>
2015-11-04 13:58:07 -0800

    attempt php validation

--- php/process.php
+++ php/process.php
@@ -1,13 +1,45 @@
 <?php
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$timeline = $_POST['timeline'];
-$budget = $_POST['budget'];
+// NAME
+if (empty($_POST["name"])) {
+    $errorMSG = "Name is required ";
+} else {
+	$name = $_POST['name'];
+}
+
+// EMAIL
+if (empty($_POST["email"])) {
+    $errorMSG . = "Email is required ";
+} else {
+	$email = $_POST['email'];
+}
+
+//TIMELINE
+if (empty($_POST["timeline"])) {
+    $errorMSG .= "Timeline is required ";
+} else {
+	$timeline = $_POST['timeline'];
+}
+
+// BUDGET
+if (empty($_POST["budget"])) {
+    $errorMSG .= "Budget is required ";
+} else {
+	$budget = $_POST['budget'];
+}
+
+
 $company = $_POST['company'];
+
 $website = $_POST['website'];
-$message = $_POST['message'];
- 
+
+// MESSAGE
+if (empty($_POST["message"])) {
+    $errorMSG .= "Message is required ";
+} else {
+	$message = $_POST['message'];
+ }
+
 $EmailTo = "zachnagatani@gmail.com";
 $Subject = "New Project Inquiry Received";
  
@@ -44,10 +76,14 @@ $Body .= "\n";
 $success = mail($EmailTo, $Subject, $Body, "From:".$email);
  
 // redirect to success page
-if ($success){
+if ($success && $errorMSG == ""){
    echo "success";
 }else{
-    echo "invalid";
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
 }
 
 ?>

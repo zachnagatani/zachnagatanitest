4adf22f24709a4f0f1341ef90597a37943e3acd3
znagatani <znagatani@gmail.com>
2015-11-04 00:04:19 -0800

    make contact form utilize ajax

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ function submitForm(){
  
     $.ajax({
         type: "POST",
-        url: "php/form-process.php",
+        url: "php/process.php",
         data: "name=" + name + "&email=" + email + "&timeline=" + timeline  + "&budget=" + budget + "&company=" + company + "&website=" + website + "&message=" + message,
         success : function(text){
             if (text == "success"){
